802e380ad47d041631a4f58b886c32ab8ec5f79f
RCRamsey <72559630+RCRamsey@users.noreply.github.com>
2021-06-13 11:56:52 -0400

    drawLegend function

--- js/proportion.js
+++ js/proportion.js
@@ -27,6 +27,7 @@ omnivore.csv('data/rates_ky_jail_by_cnty.csv')
         console.log(e.target.toGeoJSON())
 
         drawMap(e.target.toGeoJSON());
+        drawLegend(e.target.toGeoJSON());
     })
 
     .on('error', function (e) {
@@ -126,7 +127,7 @@ function drawMap(data) {
     }
 
 
-    //🚩map bounds too zoomed in but it's based on this and not set zoom above. Options?
+    //🐔 map bounds too zoomed in but it's based on this and not set zoom above. Options?
     map.fitBounds(geoJsonLayers.minuteLayer.getBounds());
 
     //radius was not differing between the circles based on the value being pulled from the layer. All were drawing the same radius no matter the value
@@ -151,4 +152,100 @@ function drawMap(data) {
     L.control.layers(null, sourcesLabels, {
         collapsed: false
     }).addTo(map);
-}
+}// end drawMap()
+
+
+// //🐔 drawLegend function
+// function drawLegend(data){
+//     console.log(data);
+//     //create leaflet control for the legend
+//     const legendControl=L.control ({
+//         position: 'bottomright'
+//     });
+
+//     //when control added to map
+//     legendControl.onAdd= function (map) {
+//         const legend = L.DomUtil.get('legend');
+        
+//         //disable scrol/click functionaliaty
+//         L.DomEvent.disableScrollPropagation(legend);
+//         L.DomEvent.disableClickPropagation(legend);
+
+//         //return selection
+//         return legend;
+//     };
+
+//     legendControl.addTo(map);
+
+
+//  // 🐔 loop through all features (need to loop through data in order for remaning lines of setting diameter size for circle legends to populate)
+//  data.features.forEach(function (time) {
+
+//      // for each time in a facility
+//      for (let time in = features.properties) {
+
+//          // shorthand to each value
+//          const value = features.properties["fifteen minute"];
+
+//          // if the value can be converted to a number 
+//          // the + operator in front of a number returns a number
+//          if (+value) {
+
+//              //return the value to the array
+//              dataValues.push(+value);
+//          }
+
+//      }
+//  });
+//  // verify your results!
+//  console.log(dataValues);
+
+//   // sort array
+//   const sortedValues = dataValues.sort(function (a, b) {
+//       return b - a;
+//   });
+
+//   // round highest number, use as large circle diameter
+//   const maxValue = Math.round(sortedValues[0] / 1000) * 1000;
+
+//   // calc diameters
+//   const largeDiameter = calcRadius(maxValue) * 2,
+//       smallDiameter = largeDiameter / 2;
+
+//   // select circles container and set height
+//   $(".legend-circles").css('height', largeDiameter.toFixed());
+
+//   // set width and height for large circle (rounded whole number)
+//   $('.legend-large').css({
+//       'width': largeDiameter.toFixed(),
+//       'height': largeDiameter.toFixed()
+//   });
+//   // set width and height for small circle and position
+//   $('.legend-small').css({
+//       'width': smallDiameter.toFixed(),
+//       'height': smallDiameter.toFixed(),
+//       'top': largeDiameter - smallDiameter,
+//       'left': smallDiameter / 2
+//   })
+
+//   // label max and median value
+//   $(".legend-large-label").html(maxValue.toLocaleString());
+//   $(".legend-small-label").html((maxValue / 2).toLocaleString());
+
+//   // adjust position of large based on size of circle
+//   $(".legend-large-label").css({
+//       'top': -11,
+//       'left': largeDiameter + 30,
+//   });
+
+//   // adjust the position of small based on size of circle
+//   $(".legend-small-label").css({
+//       'top': smallDiameter - 11,
+//       'left': largeDiameter + 30
+//   });
+
+//   // insert hr elements and use to connect value label to top of each circle
+//   $("<hr class='large'>").insertBefore(".legend-large-label")
+//   $("<hr class='small'>").insertBefore(".legend-small-label").css('top', largeDiameter - smallDiameter - 8);
+    
+// }
