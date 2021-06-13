0d07078a60180113d7f592dec284eb114222ab6c
RCRamsey <72559630+RCRamsey@users.noreply.github.com>
2021-06-13 15:20:48 -0400

    add cost to countUp timer js

--- js/countup.js
+++ js/countup.js
@@ -1,22 +1,24 @@
 
-// // hour + minutes + seconds passed
-// var timerVar= setInterval(countTimer, 1000);
-// var totalSeconds = 0;
-// function countTimer(){
-//     ++totalSeconds;
-//     var hour = Math.floor(totalSeconds / 3600); //count hour
-//     var minute = Math.floor((totalSeconds- hour * 3600 ) / 60); // count minute, subtract hours seconds from total seconds
-//     var seconds = totalSeconds - (hour *3 600 + minute * 60); //count seconds, subtract hours and minutes seconds from total seconds
-//     document.getElementById("timer").innerHTML = hour + ":" + minute + ":" + seconds;// put result in div element called
-// }
-
 // minutes + seconds passed
 var timerVar = setInterval(countTimer, 1000);
 var totalSeconds = 0;
 
 function countTimer() {
     ++totalSeconds;
+    //math.floor function returns largest integer less than or equal to given number
+    //in this instance example if totalSeconds = 100/60 number returned will be 1
     var minute = Math.floor((totalSeconds) / 60); // count minute
     var seconds = totalSeconds - (minute * 60); //count seconds, subtract minutes seconds from total seconds
-    document.getElementById("timer").innerHTML = minute + ":" + seconds; // put result in div element called
+    document.getElementById("timer").innerHTML = minute + ":" + seconds; // put result in div element called timer
+
+    //cost of call
+    var callCost = 3.32 //start call cost at first minute fee 3.32 for Henderson County
+
+    if (minute > 1){ //at roll over to second minute
+        callCost = (3.32 + (minute * 0.57)); //each additional minute costs 0.57
+    } else {
+        callCost = 3.32 //if less than 1 cost only 3.32
+    }
+    document.getElementById("cost").innerHTML = callCost; //put result in div element called cost
 }
+
