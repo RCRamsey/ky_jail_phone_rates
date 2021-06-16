e5eaab877342255fe93bacd5a7db803264bf6358
RCRamsey <72559630+RCRamsey@users.noreply.github.com>
2021-06-16 07:40:20 -0400

    Remove excess comments; add descriptor comments for new js

--- js/proportion-v2.js
+++ js/proportion-v2.js
@@ -75,26 +75,26 @@ function drawMap(data) {
     data.features.forEach(function(feature) {
         const props = feature.properties
         if (props['first minute'] != null) {
-            lz['first minute'].values.push(+props['first minute'])
+            lz['first minute'].values.push(+props['first minute']) //push values into empty values array above
         }
     })
 
-  lz['first minute'].average = lz['first minute'].values.reduce(function (sum, value) {
-        return sum + value;
-    }, 0) / lz['first minute'].values.length;
+    //calculate average of values pushed into the array using:
+    //.reduce reduces the array to a single value based on a provided a function occurring for each value (left-right) in the array. Then returns the value of function stored. This does not change the original array.
+    //sum of the array and values from the array are passed into the function and the sum is added to each value and returned.
+    //.length calculates how many numbers in the array
+    //Returned value from .reduce function (all newly calculated array values) are then divided by how many numbers are in the array. 
+    
+  lz['first minute'].average = 
+    lz['first minute'].values.reduce(function (sum, value) {
+            return sum + value;
+        }, 0) / lz['first minute'].values.length; 
 
 console.log(lz['first minute'].average)
 
 
-
-
-    /////////////////////////////////////////////////////////////////////////////////////////////
-    //dynamically create variable names for each geoJson layer and store as property names with geoJsonLayers object to access those layers and add to the map.
-    // Define the desired layers and their colors to place
-    // as JS object
-   
-    // // Build empty JS object
-    // var geoJsonLayers = {};
+    ///////////////////////////////////////////////////////////////////////////////////////
+    //define common Style for proportional symbols to be displayed
 
     var commonStyles = {
         weight: 1,
@@ -102,38 +102,22 @@ console.log(lz['first minute'].average)
         opacity: 1
     };
 
-    // Loop through first object
-    //assign a var layer for each within layerInfo of objects by looping
-    // for (var layer in layerInfo) {
+    // create circle leaflet layer from data received as a parameter in drawMap function
 
-        // 🌩 Use layerInfo as a lookup for property names and desired symbology and create the layers with the below method.
         const cost = L.geoJson(data, {
 
             pointToLayer: function (feature, latlng) {
                 // console.log(latlng)
                 return L.circleMarker(latlng, commonStyles);
             },
-            // // exist in our first object
-            // filter: function (feature) {
-    
-            //     if (feature.properties[layerInfo[layer].source]) {
-            //         console.log(feature)
-            //         return feature;
-            //     }
-            // },
             // and match the style given in first object and with getRadius()
             style: function (feature) {
-                // const x = feature.properties[layerInfo[layer].source]
-                // console.log(feature)
-                // // 🌩 how to replace or remove the $ character to make a number
+                //console.log(feature)
+
                 return {
                     color: lz['first minute'].color,
                     fillColor: lz['first minute'].color,
-                //     radius: getRadius(x)
-
-                //radius was not populating any differently using the x var. I removed the $ from the csv and used:
                     radius: getRadius(feature.properties['first minute'])
-                //or could remove $ on front end of data and use:
                 }
 
             },
@@ -144,7 +128,7 @@ console.log(lz['first minute'].average)
                         fillColor: 'white'
                     });
                     layer.on('mouseout', function(e){
-                        //🐔original style for layer is now found where? need to ref here in order to reset style after mouse out.
+
                         // layer.setStyle(lz.color)/
 
                         e.target.setStyle({
@@ -162,7 +146,7 @@ console.log(lz['first minute'].average)
                 }
 
                // console.log(props.Facility);
-               //🐔 cost will vary based on layer used, need loop to decide if only single layer on?
+               //cost will vary based on layer used
                 var popup = `Name: ${props.Facility}<br>
                             Provider: ${props.Provider}<br>
                             Rate first minute:  ${props['first minute']}, ${lz['first minute'].compare}% of average`
@@ -177,32 +161,11 @@ console.log(lz['first minute'].average)
     } // end drawMap()
 
 
-    
-
-    //radius was not differing between the circles based on the value being pulled from the layer. All were drawing the same radius no matter the value
-    // function getRadius(val) {
-    //     var radius = Math.sqrt(val / Math.PI);
-    //     return radius * 10;
-    // }
-
     // altered csv to remove $ and used: 
     function getRadius(area) {
         var radius = Math.sqrt(area/Math.PI);
         return radius * 20;
     }
-
-    //leaflet layer control with label text styled
-    // var sourcesLabels = {
-    //     "<b style ='color:#000000'>1st Minute</b>": geoJsonLayers.minuteLayer,
-    //     "<b style ='color:#9c7b66'>Additional Minute</b>": geoJsonLayers.addMinuteLayer,
-    //     "<b style ='color:#d16706'>Fifteen Minute</b>": geoJsonLayers.fifteenMinuteLayer
-    // }
-
-    // L.control.layers(null, sourcesLabels, {
-    //     collapsed: false
-    // }).addTo(map);
-
-
 
 // // drawLegend function
 // function drawLegend(data){
@@ -226,75 +189,3 @@ console.log(lz['first minute'].average)
 
 //     legendControl.addTo(map);
 
-
-//  // 🐔 loop through all features (do I need to create the layerInfo to loop through again?)
-//  data.features.forEach(function (time) {
-
-//      // for each time in a facility
-//      for (let time in = features.properties) {
-
-//          // shorthand to each value
-//          const value = features.properties["fifteen minute"];
-
-//          // if the value can be converted to a number 
-//          // the + operator in front of a number returns a number
-//          if (+value) {
-
-//              //return the value to the array
-//              dataValues.push(+value);
-//          }
-
-//      }
-//  });
-//  // verify your results!
-//  console.log(dataValues);
-
-//   // sort array
-//   const sortedValues = dataValues.sort(function (a, b) {
-//       return b - a;
-//   });
-
-//   // round highest number, use as large circle diameter
-//   const maxValue = Math.round(sortedValues[0] / 1000) * 1000;
-
-//   // calc diameters
-//   const largeDiameter = calcRadius(maxValue) * 2,
-//       smallDiameter = largeDiameter / 2;
-
-//   // select circles container and set height
-//   $(".legend-circles").css('height', largeDiameter.toFixed());
-
-//   // set width and height for large circle (rounded whole number)
-//   $('.legend-large').css({
-//       'width': largeDiameter.toFixed(),
-//       'height': largeDiameter.toFixed()
-//   });
-//   // set width and height for small circle and position
-//   $('.legend-small').css({
-//       'width': smallDiameter.toFixed(),
-//       'height': smallDiameter.toFixed(),
-//       'top': largeDiameter - smallDiameter,
-//       'left': smallDiameter / 2
-//   })
-
-//   // label max and median value
-//   $(".legend-large-label").html(maxValue.toLocaleString());
-//   $(".legend-small-label").html((maxValue / 2).toLocaleString());
-
-//   // adjust position of large based on size of circle
-//   $(".legend-large-label").css({
-//       'top': -11,
-//       'left': largeDiameter + 30,
-//   });
-
-//   // adjust the position of small based on size of circle
-//   $(".legend-small-label").css({
-//       'top': smallDiameter - 11,
-//       'left': largeDiameter + 30
-//   });
-
-//   // insert hr elements and use to connect value label to top of each circle
-//   $("<hr class='large'>").insertBefore(".legend-large-label")
-//   $("<hr class='small'>").insertBefore(".legend-small-label").css('top', largeDiameter - smallDiameter - 8);
-    
-// }
